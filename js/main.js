13d1ad97bda07513fc80d3178127f609cab83b4f
bananahotdog35 <franklinpyke@gmail.com>
2020-11-25 20:29:49 +1100

    2 jokes same button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
 console.log("connected")
+$('#reveal').hide();
+let count = 0;
+
+const jokes = [
+    {
+        question: "how do robots eat guocamole",
+        answer: "with computer chips"
+    },
+    {
+        question: "how did Darth Vader know what Luke was getting for christmas",
+        answer: "he felt his presents"
+    }
+]
 
 const generateJoke= function () {
-    console.log("I do poos")
+    let question = jokes[count].question
+    let answer = jokes[count].answer
+
+    questionparagraph = `<p id="question-${count}">${question}</p>`
+    answerparagraph = `<p id="answer-${count}">${answer}</p>`
+
+    $("#joke-container").append(questionparagraph)
+    $("#joke-container").append(answerparagraph)
+    $(`#answer-${count}`).hide()
+    
+    
+    // buttons
+    $('#generate').hide();
+    $('#reveal').show();
+
+    count ++;
 }
 
+const revealAnswer = function () {
+    $(`#answer-${count - 1}`).show()
+
+    //buttons
+    $('#reveal').hide();
+    $('#generate').show();
+};
 
 $("#generate"). on("click", generateJoke)
+$("#reveal"). on("click", revealAnswer)
